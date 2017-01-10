ccfd730974560bc95449b7e02e187d870fcf5adc
Titus <blahblah>
2017-01-10 08:28:22 -0500

    1st commit

---
+++ BasicConsole1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BasicConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int runningTotal = 0;
+			bool keepGoing = true;
+
+			while (keepGoing)
+			{ 
+				//prompt for minutes exercised
+				Console.Write("Enter minutes of whatever, or type \"quit\" to exit: ");
+
+				string entry = Console.ReadLine();
+
+				if (entry == "quit")
+				{
+					keepGoing = false;
+				}
+				else
+				{
+					int minutes = int.Parse(entry);
+					runningTotal = runningTotal + minutes;
+
+
+					//add minites to running total
+
+
+					//display total minutes
+					Console.WriteLine("you've entered " + runningTotal + " total minutes");
+
+
+					//repeat until quit
+				}
+				Console.Write("Goodbye");
+			}
+		}
+	}
+}
